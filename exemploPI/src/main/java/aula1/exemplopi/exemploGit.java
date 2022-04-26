16cd9884d626b881fb5e309da6aa387631f68695
Your Name <you@example.com>
2022-04-25 22:07:51 -0300

    criaçao de novo projeto java

--- exemploPI/src/main/java/aula1/exemplopi/exemploGit.java
+++ exemploPI/src/main/java/aula1/exemplopi/exemploGit.java
@@ -16,6 +16,7 @@ public class exemploGit {
     public static void main(String[] args) {
         int soma = 1 + 1;
         System.out.println("resultado soma: " + soma);
+        System.out.println("TESTE");
     }
     
 }
